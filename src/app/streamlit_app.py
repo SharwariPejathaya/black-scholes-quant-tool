495b2ce6974ac15ba7f129e228195a76518360a0
SharwariPejathaya <sharwaripejathaya@gmail.com>
2026-02-06 19:57:12 +0530

    Fix Streamlit Cloud imports by explicitly adding src to sys.path

--- src/app/streamlit_app.py
+++ src/app/streamlit_app.py
@@ -10,7 +10,15 @@ st.write("✅ App rendered successfully")
 
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..", "..")
+)
+SRC_PATH = os.path.join(PROJECT_ROOT, "src")
+
+if SRC_PATH not in sys.path:
+    sys.path.insert(0, SRC_PATH)
+
 
 import numpy as np
 import streamlit as st
